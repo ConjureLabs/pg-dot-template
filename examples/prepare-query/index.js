2441a0b08eb0004882190aa63cadb38c382d88e7
tmarshall <timothyjmarshall@gmail.com>
2019-12-31 10:51:50 -0800

    +comments

--- examples/prepare-query/index.js
+++ examples/prepare-query/index.js
@@ -1,10 +1,13 @@
 const pgDotTemplate = require('@conjurelabs/pg-dot-template')
 
+// required setup - appends handlers to dot-template
 pgDotTemplate.setup()
 
 async function main() {
+  // pulls in template
   const template = pgDotTemplate('select-accounts')
 
+  // replace expressions
   const query = await template({
     emailMatch: '%@gmail.com',
     idStart: 4
